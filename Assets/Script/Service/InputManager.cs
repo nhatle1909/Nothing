28a70219ce419949bc87d83faa55936f439175d2
nle75234 <nle75234@gmail.com>
2024-01-03 22:35:36 +0700

    Add rotate camera following Y Axis | Add Inventory UI & Change camera volume

--- Assets/Script/Service/InputManager.cs
+++ Assets/Script/Service/InputManager.cs
@@ -13,6 +13,7 @@ namespace Assets.Script
         public bool Interact { get; private set; }
         public bool Running { get; private set; }
         public bool EscSetting { get; private set; }
+        public bool Inventory { get; private set; }
 
         private InputActionMap _currentMap;
         private InputAction _moveAction;
@@ -20,9 +21,10 @@ namespace Assets.Script
         private InputAction _runningAction;
         private InputAction _interactAction;
         private InputAction _escAction;
+        private InputAction _inventoryAction;
         private void Awake()
         {
-            if (instance == null) 
+            if (instance == null)
             {
                 instance = this;
             }
@@ -34,43 +36,49 @@ namespace Assets.Script
             _runningAction = _currentMap.FindAction("Running");
             _interactAction = _currentMap.FindAction("Interact");
             _escAction = _currentMap.FindAction("System");
+            _inventoryAction = _currentMap.FindAction("Inventory");
 
             _moveAction.performed += onMove;
             _lookAction.performed += onLook;
             _runningAction.performed += onRunning;
             _interactAction.performed += onInteract;
             _escAction.performed += onSystemSetting;
-
+            _inventoryAction.performed += onInventory;
 
             _runningAction.canceled += outRunning;
             _interactAction.canceled += outRunning;
-         
-      
+
+
+
 
         }
         public void onMove(InputAction.CallbackContext context)
         {
             Move = context.ReadValue<Vector3>();
-         
+
         }
         public void onRunning(InputAction.CallbackContext context)
         {
             Running = true;
 
         }
-       
+
         public void onLook(InputAction.CallbackContext context)
         {
             Look = context.ReadValue<Vector2>();
-            
+
         }
-        public void onSystemSetting(InputAction.CallbackContext context) 
+        public void onSystemSetting(InputAction.CallbackContext context)
         {
             if (EscSetting == true) EscSetting = false;
             else EscSetting = true;
-           
+
         }
-        
+        public void onInventory(InputAction.CallbackContext context)
+        {
+            if (Inventory == true) Inventory = false;
+            else Inventory = true;
+        }
         public void onInteract(InputAction.CallbackContext context)
         {
             Interact = true;
@@ -82,10 +90,10 @@ namespace Assets.Script
         {
             Running = false;
         }
-        public void outInteract(InputAction.CallbackContext context) 
+        public void outInteract(InputAction.CallbackContext context)
         {
             Interact = false;
         }
-       
+
     }
 }
---
+++ Assets/Script/Service/RenderCamera.cs
@@ -0,0 +1,29 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//namespace Assets.Script.Service
+//{
+//    public class RenderCamera : MonoBehaviour
+//    {
+
+//        // Use this for initialization
+//        public Camera camera;
+//        void Start()
+//        {
+//            camera = Camera.main;
+//        }
+
+//        // Called for each renderer that became visible
+//        void OnBecameVisible()
+//        {
+//            // Add the renderer to a list of visible objects
+//            List<Renderer> visibleRenderers = new List<Renderer>();
+//            visibleRenderers.Add(renderer);
+
+//            // Set the camera's culling mask to only render these objects
+//            camera.cullingMask = 1 << LayerMask.NameToLayer("Visible"); // Assuming objects are on a layer named "Visible"
+//        }
+
+//    }
+//}
